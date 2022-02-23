3718b979f92ac3990a53b108384974e6d77ce758
Zero <iamvamsi999@gmail.com>
2022-02-24 01:08:24 +0530

    Added DBurl for heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to mongodb
-mongoose.connect('mongodb://localhost/ecommerceApi');
+// mongoose.connect('mongodb://localhost/ecommerceApi');
+const dbUrl = 'mongodb+srv://UsernameZero:UsernameZero@cluster0.oe1eg.mongodb.net/ecommerceApi';
+mongoose.connect( dbUrl , {useNewUrlParser: true});
 
 const db = mongoose.connection; // storing the data from mongoDB to initialized variable
 
